08875c9f0b6a8aa40061ffaff07bb8716b509218
cordilop <molineroperez9@gmail.com>
2026-02-18 00:36:05 -0800

    segundo

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-main').classList.toggle('show');
 });
+
+
+ScrollReveal().reveal('.showcase');
+
+ScrollReveal().reveal('.news-cards', {delay: 500 });
+
+ScrollReveal().reveal('.cards-banner-one', {delay: 500 });
+
+ScrollReveal().reveal('.cards-banner-two', {delay: 500 });
